62af0099348936e959b913ea16733cc41958d662
Troll Ádám <adam.troll@levoolabs.com>
2021-02-17 22:05:42 +0100

    Basic laravel installation

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.master')
+
+@section('content')
+<p>Hello from blade</p>
+@endsection
---
+++ resources/views/layout/_header.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container">
+        <a href="#" class="navbar-brand">Bloggr</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="mainNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Other</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
